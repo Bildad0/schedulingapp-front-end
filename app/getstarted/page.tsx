e220bd9ae3f6501312fb84c3bc77c19f90a0241a
BildadOwuor <bildadowuor@gmail.com>
2023-05-28 16:11:36 +0300

    updates

--- app/getstarted/page.tsx
+++ app/getstarted/page.tsx
@@ -1,7 +1,10 @@
-function GetStarted(){
-    return (
-        <main className="w-fit  p-20 min-h-screen ">
+// "use client"
+
+
+// function GetStarted(){
+//     return (
+//         <main className="w-fit  p-20 min-h-screen ">
        
-        </main>
-    )
-}
+//         </main>
+//     )
+// }
